c4aa93878c807cfbcd325b64c3eeda8309248013
210752@utxicotepec.edu.mx <210752@utxicotepexc.edu.mx>
2024-11-19 11:57:08 -0600

    Practica24
    Creacion de un nuew proyect de Toktik

---
+++ toktik_210752/lib/infrastructure/models/local_video_model.dart
@@ -0,0 +1,13 @@
+
+class LocalVideoModel {
+  final String name;
+  final String videoUrl;
+  final int likes;
+  final int views;
+
+  LocalVideoModel(
+      {required this.name,
+      required this.videoUrl,
+      this.likes = 0,
+      this.views = 0});
+}
